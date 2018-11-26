1642f5e8edc9b146a4727b9180c154ef96264951
dpoulson <darren.poulson@gmail.com>
2018-11-26 21:10:20 +0000

    Adding the LICENSE

--- SabertoothPacketSerial/__init__.py
+++ SabertoothPacketSerial/__init__.py
@@ -2,6 +2,7 @@ from .SabertoothPacketSerial import SabertoothPacketSerial
 
 from .version import __version__
 
+name = "SabertoothPacketSerial"
 __author__ = 'Darren Poulson'
 __license__ = 'GPL'
 
