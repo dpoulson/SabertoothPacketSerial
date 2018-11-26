a137f10309ee4bd035711378aa7dfc037f8ddb74
dpoulson <darren.poulson@gmail.com>
2018-11-26 21:48:38 +0000

    Bumped version and added long description

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='SabertoothPacketSerial',
-      version='0.2.1',
+      version='0.2.2',
       description='Library for Dimenson Engineering Sabertooth speed controller',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/dpoulson/SabertoothPacketSerial',
       author='Darren Poulson',
       author_email='darren.poulson@gmail.com',
       license='GPL',
-      packages=['SabertoothPacketSerial'],
+      packages=setuptools.find_packages(),
       zip_safe=False)
