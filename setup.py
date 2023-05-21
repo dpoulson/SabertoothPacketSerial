9bde810d46c99d1ddf16768be2936799a9b78acc
Darren Poulson <darren.poulson@gmail.com>
2023-05-21 23:23:49 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='SabertoothPacketSerial',
-      version='0.2.2',
+      version='0.2.3',
       description='Library for Dimenson Engineering Sabertooth speed controller',
       long_description=long_description,
       long_description_content_type="text/markdown",
